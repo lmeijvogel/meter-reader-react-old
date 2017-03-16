f074ea63f3a9abc06a63f498f57ca816e1f4798e
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2017-03-16 16:04:44 +0100

    Don't let the graphs take up too much space

--- src/period-usage-display.jsx
+++ src/period-usage-display.jsx
@@ -11,8 +11,10 @@ export default class PeriodUsageDisplay extends Component {
   render() {
     const labels = this.labels();
 
+    // Apparently, Chart.js doesn't understand 'height' and 'maxHeight' correctly, but only handles 'width' and 'max-width'.
+    // The maxWidth here corresponds to filling a single screen (vertically) on my laptop.
     return (
-      <div>
+      <div style={{maxWidth: '500px'}}>
         <Chart label="Gas" labels={labels} data={this.props.usage} fieldName="gas" color="#f0ad4e" onClick={this.onClick.bind(this)}></Chart>
         <Chart label="Stroom" labels={labels} data={this.props.usage} fieldName="stroom_totaal" color="#428bca" onClick={this.onClick.bind(this)}></Chart>
       </div>
