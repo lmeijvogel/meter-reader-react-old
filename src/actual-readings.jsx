4efc8c9c35ea55b5945266cf5b420ae59ad88557
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2017-03-23 14:51:07 +0100

    Improve layout of 'ActualReadings' (a bit)

--- src/actual-readings.jsx
+++ src/actual-readings.jsx
@@ -8,23 +8,29 @@ export default class ActualReadings extends Component {
   }
 
   render() {
+    const {stroom_dal, stroom_piek, gas} = this.props;
+
     return (
-      <table className="numeric-data">
-        <thead>
-        <tr>
-          <th>Meterstand stroom dal(kWh)</th>
-          <th>Meterstand stroom piek(kWh)</th>
-          <th>Meterstand gas (m<sup>3</sup>)</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>{this.props.stroom_dal}</td>
-          <td>{this.props.stroom_piek}</td>
-          <td>{this.props.gas}</td>
-        </tr>
-        </tbody>
-      </table>
+      <div>
+        <h3>Meterstanden</h3>
+
+        <table className="numeric-data">
+          <thead>
+          <tr>
+            <th>Dal (kWh)</th>
+            <th>Piek (kWh)</th>
+            <th>Gas (m<sup>3</sup>)</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>{stroom_dal}</td>
+            <td>{stroom_piek}</td>
+            <td>{gas}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
