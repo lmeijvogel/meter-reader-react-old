2e4643a365d121eb6dcf6868204e4781ba78e550
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2017-04-13 11:01:01 +0200

    Fix error on period-usage-display classname.

--- src/period-usage-display.jsx
+++ src/period-usage-display.jsx
@@ -16,7 +16,7 @@ export default class PeriodUsageDisplay extends Component {
     const data =  dataShifter.call(this.props.usage, this.positionInData.bind(this));
 
     return (
-      <div class="period-usage-display" className={'period-usage-display' + (this.props.enabled ? '' : ' disabled')}>
+      <div className={'period-usage-display' + (this.props.enabled ? '' : ' disabled')}>
         <Chart label="Gas" labels={labels} data={data} fieldName="gas" color="#f0ad4e" onClick={this.onClick.bind(this)} tooltipLabelBuilder={this.tooltipLabel.bind(this)}></Chart>
         <Chart label="Stroom" labels={labels} data={data} fieldName="stroom_totaal" color="#428bca" onClick={this.onClick.bind(this)} tooltipLabelBuilder={this.tooltipLabel.bind(this)}></Chart>
       </div>
