c815c2267b0ea55f81891b839e723cb11f3f212d
Lennaert Meijvogel <l.meijvogel@websend.nl>
2017-10-07 21:52:56 +0200

    Add helpful comment that should explain where CSS is added by Webpack.

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import EnergyUsageApp from './energy-usage-app.jsx';
+
+// Importing these here will include them on the resulting page
 import energieStyles from './styles/energie.css';
 import mainStyles from './styles/main.css';
 import milligramStyles from './styles/milligram.min.css';
