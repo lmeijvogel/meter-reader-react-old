2092fd51a1228744eabf4ca2e565919a4c2004df
Lennaert Meijvogel <l.meijvogel@websend.nl>
2017-10-17 22:16:06 +0200

    Fix stylesheet imports for typescript.

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import EnergyUsageApp from './energy-usage-app';
-/*import energieStyles from './styles/energie.css';*/
-/*import mainStyles from './styles/main.css';*/
-/*import milligramStyles from './styles/milligram.min.css';*/
+const energieStyles = require('./styles/energie.css');
+const mainStyles = require('./styles/main.css');
+const milligramStyles = require('./styles/milligram.min.css');
 
 const appContainer = document.querySelector("#app");
 
