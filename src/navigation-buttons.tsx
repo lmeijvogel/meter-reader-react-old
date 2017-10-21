802e79841eecf4f07300a97fc0cd14f877035ec8
Lennaert Meijvogel <l.meijvogel@websend.nl>
2017-10-21 20:54:39 +0200

    Extract function for navigation buttons for all periods.

--- src/navigation-buttons.tsx
+++ src/navigation-buttons.tsx
@@ -11,91 +11,76 @@ interface IProps {
 
   onSelect: (any) => void;
 }
+
+interface NavigationButton {
+  name : string;
+  period: PeriodDescription;
+}
+
+interface NavigationButtonParams {
+  previous: NavigationButton;
+  next: NavigationButton;
+  up?: NavigationButton;
+}
+
 export default class NavigationButtons extends Component<IProps, {}> {
   render() {
     const periodDescription = this.props.periodDescription;
-    const today = DayDescription.today();
-
-    const todayButton = <ChangePeriodButton label="Today" onClick={() => this.newPeriod(today)} enabled={this.props.enabled} className="navigation-button" />
 
     if (periodDescription instanceof YearDescription) {
       const yearDescription = periodDescription as YearDescription;
 
-      const previousYear = yearDescription.previous();
-      const nextYear = yearDescription.next();
-
-      return <div className="container">
-        <div className="row">
-          <div className="column column-md-50 column-sm-100">
-            <ChangePeriodButton label="Previous year" onClick={() => this.newPeriod(previousYear)} enabled={this.props.enabled} className="navigation-button" />
-          </div>
-          <div className="column column-md-50 column-sm-100">
-            <ChangePeriodButton label="Next year" onClick={() => this.newPeriod(nextYear)} enabled={this.props.enabled} className="navigation-button" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="column column-100">
-            {todayButton}
-          </div>
-        </div>
-      </div>;
+      return this.navigationButtons({
+        previous: {name: 'Jaar terug', period: yearDescription.previous()},
+        next: {name: 'Jaar vooruit', period: yearDescription.next()}
+      });
     } else if (periodDescription instanceof MonthDescription) {
       const monthDescription = periodDescription as MonthDescription;
 
-      const previousMonth = monthDescription.previous();
-      const nextMonth = monthDescription.next();
-      const currentYear = monthDescription.up();
-
-      return <div className="container">
-        <div className="row">
-          <div className="column column-md-50 column-sm-100">
-            <ChangePeriodButton label="Previous month" onClick={() => this.newPeriod(previousMonth)} enabled={this.props.enabled} className="navigation-button" />
-          </div>
-          <div className="column column-md-50 column-sm-100">
-            <ChangePeriodButton label="Next month" onClick={() => this.newPeriod(nextMonth)} enabled={this.props.enabled} className="navigation-button" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="column column-100">
-            <ChangePeriodButton label="Up to year" onClick={() => this.newPeriod(currentYear)} enabled={this.props.enabled} className="navigation-button" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="column column-100">
-            {todayButton}
-          </div>
-        </div>
-      </div>;
+      return this.navigationButtons({
+        previous: {name: 'Maand terug', period: monthDescription.previous()},
+        next: {name: 'Maand vooruit', period: monthDescription.next()},
+        up: {name: "Naar jaar", period: monthDescription.up()}
+      });
     } else if (periodDescription instanceof DayDescription) {
       const dayDescription = periodDescription as DayDescription;
 
-      const previousDate = dayDescription.previous();
-      const nextDate = dayDescription.next();
-      const currentMonth = dayDescription.up();
-
-      return <div className="container">
-        <div className="row">
-          <div className="column column-md-50 column-sm-100">
-            <ChangePeriodButton label="Previous day" onClick={() => this.newPeriod(previousDate)} enabled={this.props.enabled} className="navigation-button" />
-          </div>
-          <div className="column column-md-50 column-sm-100">
-            <ChangePeriodButton label="Next day" onClick={() => this.newPeriod(nextDate)} enabled={this.props.enabled} className="navigation-button" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="column column-100">
-            <ChangePeriodButton label="Up to month" onClick={() => this.newPeriod(currentMonth)} enabled={this.props.enabled} className="navigation-button" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="column column-100">
-            {todayButton}
-          </div>
-        </div>
-      </div>;
+      return this.navigationButtons({
+        previous: {name: "Dag terug", period: dayDescription.previous()},
+        next: {name: "Dag vooruit", period: dayDescription.next()},
+        up: {name: "Naar maand", period: dayDescription.up()}
+      });
     }
 
     return <div />;
+  }
+
+  navigationButtons({previous, next, up} : NavigationButtonParams) : JSX.Element {
+    const today = DayDescription.today();
+
+    const todayButton = <ChangePeriodButton label="Today" onClick={() => this.newPeriod(today)} enabled={this.props.enabled} className="navigation-button" />
+
+    return <div className="container">
+             <div className="row">
+               <div className="column column-md-50 column-sm-100">
+                 <ChangePeriodButton label={"< "+ previous.name} onClick={() => this.newPeriod(previous.period)} enabled={this.props.enabled} className="navigation-button" />
+               </div>
+               <div className="column column-md-50 column-sm-100">
+                 <ChangePeriodButton label={next.name + " >"} onClick={() => this.newPeriod(next.period)} enabled={this.props.enabled} className="navigation-button" />
+               </div>
+             </div>
+             {up && <div className="row">
+                      <div className="column column-100">
+                        <ChangePeriodButton label={up.name} onClick={() => this.newPeriod(up.period)} enabled={this.props.enabled} className="navigation-button" />
+                      </div>
+                    </div>}
+
+             <div className="row">
+               <div className="column column-100">
+                 {todayButton}
+               </div>
+             </div>
+           </div>;
   }
 
   newPeriod(period : PeriodDescription) {
