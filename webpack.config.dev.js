b004b42f4df492a5e352283bc546850b4086f85f
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2018-09-18 21:37:56 +0200

    Fix build errors and lookup warnings.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,52 +1,54 @@
-var path = require('path');
-var webpack = require('webpack');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // `CheckerPlugin` is optional. Use it if you want async error reporting.
 // We need this plugin to detect a `--watch` mode. It may be removed later
 // after https://github.com/webpack/webpack/issues/3460 will be resolved.
-const { CheckerPlugin } = require('awesome-typescript-loader')
+const { CheckerPlugin } = require("awesome-typescript-loader");
 
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: "./build/",
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    loaders: [
-        {
-          test: /.css$/,
-          loader: 'style-loader!css-loader'
-        },
-        {
-        test: /.jsx?$|.tsx?$/,
-        loader: 'awesome-typescript-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: require('html-webpack-template'),
+    entry: "./src/index.tsx",
+    output: {
+        path: __dirname + "/build/",
+        filename: "bundle.js",
+        publicPath: "/",
+    },
+    module: {
+        loaders: [
+            {
+                test: /.css$/,
+                loader: "style-loader!css-loader",
+            },
+            {
+                test: /.jsx?$|.tsx?$/,
+                loader: "awesome-typescript-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["es2015", "react"],
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: require("html-webpack-template"),
 
-      title: "Energie (dev)",
-      meta: [{
-        name: "viewport",
-        content: "width=device-width, initial-scale=1"
-      }],
-      appMountId: 'app'
-    }),
-    new CheckerPlugin()
-  ],
+            title: "Energie (dev)",
+            meta: [
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1",
+                },
+            ],
+            appMountId: "app",
+        }),
+        new CheckerPlugin(),
+    ],
 
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.jsx', '.json']
-  }
-}
+    resolve: {
+        extensions: [".ts", ".js", ".tsx", ".jsx", ".json"],
+    },
+};
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,60 +1,62 @@
-var path = require('path');
-var webpack = require('webpack');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: "./dist/",
-    filename: 'bundle-[hash].js',
-    publicPath: '/'
-  },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$|.tsx?$/,
-        loader: 'awesome-typescript-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-      {
-        test: /.css$/,
-        loader: 'style-loader!css-loader'
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist'], {
-      paths: ['./dist'],
-      exclude: ['login.html'],
-      verbose: false,
-      dry: false
-    }),
-    new webpack.DefinePlugin({ // <-- key to reducing React's size
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin(), // Minify everything
-    new webpack.optimize.AggressiveMergingPlugin(), // Merge chunks
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: require('html-webpack-template'),
+    entry: "./src/index.tsx",
+    output: {
+        path: __dirname + "/dist/",
+        filename: "bundle-[hash].js",
+        publicPath: "/",
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$|.tsx?$/,
+                loader: "awesome-typescript-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["es2015", "react"],
+                },
+            },
+            {
+                test: /.css$/,
+                loader: "style-loader!css-loader",
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(["dist"], {
+            paths: ["./dist"],
+            exclude: ["login.html"],
+            verbose: false,
+            dry: false,
+        }),
+        new webpack.DefinePlugin({
+            // <-- key to reducing React's size
+            "process.env": {
+                NODE_ENV: JSON.stringify("production"),
+            },
+        }),
+        new webpack.optimize.UglifyJsPlugin(), // Minify everything
+        new webpack.optimize.AggressiveMergingPlugin(), // Merge chunks
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: require("html-webpack-template"),
 
-      title: "Energie",
-      meta: [{
-        name: "viewport",
-        content: "width=device-width, initial-scale=1"
-      }],
-      appMountId: 'app'
-    })
+            title: "Energie",
+            meta: [
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1",
+                },
+            ],
+            appMountId: "app",
+        }),
+    ],
 
-  ],
-
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.jsx', '.json']
-  }
-}
+    resolve: {
+        extensions: [".ts", ".js", ".tsx", ".jsx", ".json"],
+    },
+};
