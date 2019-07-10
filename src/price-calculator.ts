a0d8a964b70af0d257b2329cbf58407695ca5e82
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2019-07-10 10:37:14 +0200

    Add new usage rates.

--- src/price-calculator.ts
+++ src/price-calculator.ts
@@ -26,8 +26,10 @@ type RateForDateRange = {
 
 export namespace PriceCalculator {
     // Since new rates are added only once every two years,
-    // I think the effor of building a frontend for managing these
+    // I think the effort of building a frontend for managing these
     // rates won't be worth the effort :D
+    //
+    // All rates are in given in euros, but our Money class wants cents.
     const prices: RateForDateRange[] = [
         {
             gasPrice: new Money(0.267 * 100),
@@ -35,6 +37,13 @@ export namespace PriceCalculator {
             waterPrice: new Money(0),
             validFrom: new Date(2014, 0, 1),
             validUntil: new Date(2018, 10, 11)
+        },
+        {
+            gasPrice: new Money(0.75336 * 100),
+            stroomPrice: new Money(0.22652 * 100),
+            waterPrice: new Money(1.30037 * 100 / 1000), // Incl. BTW, 1.193 excl. BTW. Rate is given on the Evides website in euros/m3, but I'd like to show in liters, so divide by 1000
+            validFrom: new Date(2018, 10, 12),
+            validUntil: new Date(2019, 10, 11)
         }
     ];
 
