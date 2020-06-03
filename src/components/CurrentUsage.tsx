1d8822e09a170cbdc1293b4a3e31a80f7437d06f
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2020-06-03 15:25:27 +0200

    Prettier.

--- src/components/CurrentUsage.tsx
+++ src/components/CurrentUsage.tsx
@@ -4,39 +4,39 @@ import { Component } from "react";
 import { LiveData } from "../models/LiveData";
 
 type IProps = {
-  liveData: LiveData | null;
-  onClick: () => void;
+    liveData: LiveData | null;
+    onClick: () => void;
 };
 
 export class CurrentUsage extends Component<IProps> {
-  render() {
-    return (
-      <table className="column column-20" onClick={this.onClick}>
-        <thead>
-          <tr>
-            <th>Current</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{this.displayableCurrent}</td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
-
-  private onClick = () => {
-    this.props.onClick();
-  };
-
-  private get displayableCurrent(): string {
-    const { liveData } = this.props;
-
-    if (liveData) {
-      return `${liveData.current * 1000} W`;
-    } else {
-      return "...";
+    render() {
+        return (
+            <table className="column column-20" onClick={this.onClick}>
+                <thead>
+                    <tr>
+                        <th>Current</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{this.displayableCurrent}</td>
+                    </tr>
+                </tbody>
+            </table>
+        );
     }
-  }
+
+    private onClick = () => {
+        this.props.onClick();
+    };
+
+    private get displayableCurrent(): string {
+        const { liveData } = this.props;
+
+        if (liveData) {
+            return `${liveData.current * 1000} W`;
+        } else {
+            return "...";
+        }
+    }
 }
